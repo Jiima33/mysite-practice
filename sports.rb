e8bb923a6f3f53e974ea8da83993cb80fdd3d3d2
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2023-01-24 02:16:17 +0000

    与えられた名前の数を出力する問題

---
+++ sports.rb
@@ -0,0 +1,14 @@
+# 球技名sが与えられチームの人数を出力します。
+s = gets.chomp
+
+if s == "baseball"
+  puts 9
+elsif s == "soccer"
+  puts 11
+elsif s == "rugby"
+  puts 15
+elsif s == "basketball"
+  puts 5
+elsif s == "volleyball"
+  puts 6
+end
